009fa54b6d3dc0d813a4e422cd4f9568f3722ce4
mistervova <MisterVova@mail.ru>
2022-06-04 12:29:59 +0300

    yjdfz dthcbz

--- task/settings/settings.py
+++ task/settings/settings.py
@@ -1,7 +1,6 @@
 import json
 
-# URL_SERVER = "https://script.google.com/macros/s/AKfycbx2Jm6G5vZLIgOYE8Bd2FWRevyn6KQKAGgEU4OTV4TUeIEYZ_RBFaJ7Ldau9MNzVTNq3w/exec"
-URL_SERVER = "https://script.google.com/macros/s/AKfycbyMVhB21t6diZPSJuToIvHujWj_7xGTMJLzUkJSoxKKsw1I/exec"
+URL_SERVER = "https://script.google.com/macros/s/AKfycbyMVhB21t6diZPSJuToIvHujWj_/exec"
 URL_WB_MAIN = "https://www.wildberries.ru/"
 URL_WB_SEARCH = "https://www.wildberries.ru/catalog/0/search.aspx?sort=popular&search="
 THREADS_COUNT = 4
